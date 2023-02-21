2b42e616042586812d5f9ae9ba24193e23a6515a
Lance Collins <lancec@microsoft.com>
2023-02-21 16:52:49 -0600

    Add start web project.

---
+++ src/Codex.Web.Wasm/Program.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Runtime.InteropServices.JavaScript;
+using System.Threading;
+using System.Threading.Tasks;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello4, Browser!");
+
+        for (int i = 0; i < 10; i++)
+        {
+            Task.Run(async () =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine($"Running thread with id: {Thread.CurrentThread.ManagedThreadId}");
+                await Task.Delay(1000);
+            });
+        }
+    }
+}
+
+public partial class MyClass
+{
+    [JSExport]
+    internal static string Greeting()
+    {
+        var text = $"Hello, World2! Greetings from {GetHRef()}";
+        Console.WriteLine(text);
+        return text;
+    }
+
+    [JSImport("window.location.href", "main.js")]
+    internal static partial string GetHRef();
+}
